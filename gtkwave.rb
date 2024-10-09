5e0bcc0fa45aab9bb6d4b97b48ab82b7832468d0
Tang Haojin <tanghaojin@outlook.com>
2024-10-10 01:20:23 +0800

    use official git repository

--- gtkwave.rb
+++ gtkwave.rb
@@ -2,8 +2,8 @@ class Gtkwave < Formula
   desc "GTKWave"
   homepage "https://gtkwave.sourceforge.net"
   license "GPL-2.0-or-later"
-  head "https://github.com/randomplum/gtkwave.git", branch: "master"
-  revision 3
+  head "https://github.com/gtkwave/gtkwave.git", branch: "master"
+  revision 1
 
   depends_on "desktop-file-utils" => :build # for update-desktop-database
   depends_on "shared-mime-info" => :build
@@ -16,7 +16,7 @@ class Gtkwave < Formula
 
   def install
     ENV["DESTDIR"] = "/"
-    system "meson", "setup", "build", "-Dintrospection=false", *std_meson_args
+    system "meson", "setup", "build", *std_meson_args
     system "meson", "compile", "-C", "build", "--verbose"
     system "meson", "install", "-C", "build"
   end
